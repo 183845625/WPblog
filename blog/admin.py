ef880215fd8bee61d05592da0295eb74428e289f
183845625 <183845625@qq.com>
2019-07-31 13:29:31 +0800

    page138

--- blog/admin.py
+++ blog/admin.py
@@ -1,39 +1,63 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 from django.urls import reverse
 from django.utils.html import format_html
 
+from WPblog.base_admin import BaseOwnerAdmin
+from WPblog.custom_site import custom_site
+from blog.adminforms import PostAdminForm
 from .models import Post, Category, Tag
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time')
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1
+    model = Post
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
+    inlines = [PostInline, ]
     
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
-    
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示用户分类"""
+
+    title = '分类过滤器'
+    parameter_name = 'ower_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
     list_display = [
-        'title', 'category', 'status', 
-        'created_time', 'operator'
+        'title', 'category', 'status',
+        'created_time', 'owner', 'operator'
     ]
     list_display_links = []
     
-    list_filter = ['category']
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
     
     actions_on_bottom = True
@@ -41,21 +65,44 @@ class PostAdmin(admin.ModelAdmin):
     
     save_on_top = True
     
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tags',
+    # )
+    #
+    fieldsets = (
+        ('基本配置', {
+            'description': '基本配置描述',
+            'fields': (('title', 'category'), 'status',)
+        }),
+        ('内容',{
+            'fields':('desc', 'content',)
+        }),
+        ('额外信息', {
+            'classes': ('collapse', ),
+            'fields': ('tag', ),
+        })
     )
-    
+
+
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id, ))
+            reverse('cus_admin:blog_post_change', args=(obj.id, ))
         )
     operator.short_description = '操作'
-    
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
+
+
+    # class Media:
+    #     css = {
+    #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+    #     }
+    #     js = ('http://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js', )
+
+
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
